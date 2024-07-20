9df43a5b652bcbd14a7764ca597c1983f8ac9788
rajeshmondal54 <rm4223536@gmail.com>
2024-07-21 01:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function capitalizeOnBlur() {
+    const inputField = document.getElementById('fname');
+	if (inputField) {
+        inputField.addEventListener('blur', function() {
+            // Convert the content to uppercase
+            inputField.value = inputField.value.toUpperCase();
+        });
+    }
+}
+capitalizeOnBlur();
